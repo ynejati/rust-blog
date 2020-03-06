ef22184c7643bb0c7c48910f9e38e1f0a9ed0016
Yousuf Nejati <yousufnejati@gmail.com>
2020-03-06 04:52:20 -0800

    App setup and error handling, WIP

---
+++ src/errors.rs
@@ -0,0 +1,24 @@
+use actix_web::error::BlockingError;
+use actix_web::web::HttpResponse;
+use diesel::result::DatabaseErrorKind::UniqueViolation;
+use diesel::result::Error::{DatabaseError, NotFound};
+use std::fmt;
+
+#[derive(Debug)]
+pub enum AppError {
+  RecordAlreadyExists,
+  RecordNotFound,
+  DatabaseError(diesel::result::Error),
+  OperationCanceled,
+}
+
+impl fmt::Display for AppError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      AppError::RecordAlreadyExists => write!(f, "This record violates a unique constraint"),
+      AppError::RecordNotFound => write!(f, "This record does not exist"),
+      AppError::DatabaseError(e) => write!(f, "Database error: {:?}", e),
+      AppError::OperationCanceled => write!(f, "The running operation was canceled"),
+    }
+  }
+}
